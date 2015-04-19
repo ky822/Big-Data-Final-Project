b7209ae4f5dcd7ee10416781d826cd4935e2e60e
Mengfei Li <ritali@172-16-206-136.DYNAPOOL.NYU.EDU>
2015-04-19 14:04:02 -0400

    new map

--- map.py
+++ map.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python
 import sys
-import string
 
 
 FARE_COL = 11
 
 
 for line in sys.stdin:
-	l=line.split(",")
-	if l[0]!="medallion" or l[4] != 'CSH' :
-		if len(l)==FARE_COL:
+    l = line.split(",")
+    if l[0] == 'medallion':
+       continue
+    elif l[0] != "medallion":
+	    if len(l) == FARE_COL:
+             if l[4] == 'CRD' or l[4] == 'UNK':
 			key = str(l[0]+','+l[1]+','+l[2]+','+l[3])
 			l[-1] = l[-1].rstrip("\n")
 			value = "\t".join(l[4:])
 			value.rstrip("\n")
-		else:
-			key = str(l[0]+','+l[1]+','+l[2]+','+l[5])
-			l[-1] = l[-1].rstrip('\n')
-			value = "\t".join(l[3:5]+l[6:])
-			value.rstrip("\n")
-	else:
-		continue
-	print "%s\t%s" %( key, value)	
+             else:
+                continue
+	    else:
+		key = str(l[0]+','+l[1]+','+l[2]+','+l[5])
+		l[-1] = l[-1].rstrip('\n')
+		value = "\t".join(l[3:5]+l[6:])
+		value.rstrip("\n")
+    print "%s\t%s" %( key, value)
